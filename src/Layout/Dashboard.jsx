bf1b332be8c3f78ca7e5431f0c2a4cd271f36970
simanto97 <saimumsimanto97@gmail.com>
2023-06-08 19:13:57 +0600

    dashboard implement

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,28 +1,102 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import { FiSidebar } from "react-icons/fi";
+import { ImHome } from "react-icons/im";
+import { GiTeacher } from "react-icons/gi";
+import { FaRegClipboard } from "react-icons/fa";
+import {
+  FcContacts,
+  FcInspection,
+  FcAddRow,
+  FcDataConfiguration,
+  FcConferenceCall,
+} from "react-icons/fc";
+// import useGetDbUser from "../hooks/useGetDbUser";
 
 const Dashboard = () => {
+  // const dbUser = useGetDbUser();
+  const isIns = true;
   return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col items-center justify-center">
+      <div className="drawer-content flex flex-col items-end justify-center">
         {/* Page content here */}
         <label
           htmlFor="my-drawer-2"
-          className="btn btn-primary drawer-button lg:hidden"
+          className="btn btn-primary drawer-button mr-8 mt-4 lg:hidden"
         >
-          Open drawer
+          <FiSidebar />
         </label>
         <Outlet />
       </div>
-      <div className="drawer-side">
+      <div className="drawer-side bg-[#00b0e4] text-red-700">
         <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-        <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
+        <ul className="menu p-4 w-80 h-full text-black text-lg">
           {/* Sidebar content here */}
+          {!isIns && (
+            <>
+              <li>
+                <Link to="/dashboard/carts">
+                  <FcInspection className="text-2xl" /> My selected class
+                </Link>
+              </li>
+              <li>
+                <Link to="/dashboard">
+                  <FcContacts className="text-2xl" />
+                  My enrolled class
+                </Link>
+              </li>
+            </>
+          )}
+          {!isIns && (
+            <>
+              <li>
+                <Link to="/dashboard/carts">
+                  <FcAddRow className="text-2xl" />
+                  Add class
+                </Link>
+              </li>
+              <li>
+                <Link to="/dashboard">
+                  <FcContacts className="text-2xl" />
+                  My classes
+                </Link>
+              </li>
+            </>
+          )}
+          {isIns && (
+            <>
+              <li>
+                <Link to="/dashboard/carts">
+                  <FcDataConfiguration className="text-2xl" />
+                  Manage classes
+                </Link>
+              </li>
+              <li>
+                <Link to="/dashboard">
+                  <FcConferenceCall className="text-2xl" />
+                  Manage users
+                </Link>
+              </li>
+            </>
+          )}
+
+          <div className="divider"></div>
           <li>
-            <a>Sidebar Item 1</a>
+            <Link to="/">
+              <ImHome className="text-2xl" /> Home
+            </Link>
           </li>
           <li>
-            <a>Sidebar Item 2</a>
+            <Link to="/instructors">
+              <GiTeacher className="text-2xl" />
+              Instructors
+            </Link>
+          </li>
+          <li>
+            <Link to="/classes">
+              <FaRegClipboard className="text-2xl" />
+              Classes
+            </Link>
           </li>
         </ul>
       </div>
--- src/pages/Dashboard/Cart/Cart.jsx
+++ src/pages/Dashboard/Cart/Cart.jsx
@@ -1,16 +1,51 @@
+import { Link } from "react-router-dom";
 import useCart from "../../../hooks/useCart";
 import { RiDeleteBack2Fill } from "react-icons/ri";
+import Swal from "sweetalert2";
 
 const Cart = () => {
   const [cart, refetch] = useCart();
+
+  const total = cart.reduce((sum, item) => sum + item.price, 0);
   const handleDelete = (item) => {
-    fetch(`${import.meta.env.VITE_HOSTING_URL}/dashboard/carts/${item._id}`).then(res=>res.json()).then(dat);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(
+          `${import.meta.env.VITE_HOSTING_URL}/dashboard/carts/${item._id}`,
+          {
+            method: "DELETE",
+          }
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              refetch();
+              Swal.fire(
+                "Deleted!",
+                "Your cart item has been deleted.",
+                "success"
+              );
+            }
+          });
+      }
+    });
   };
   return (
-    <div className="w-4/5 mx-auto overflow-x-auto">
-      <div className="flex justify-between items-center">
-        <h2 className="font-semibold text-3xl text-center my-8">Cart</h2>
-        <button className="btn btn-warning">pay</button>
+    <div className="md:w-4/5 mx-auto overflow-x-auto">
+      <div className="uppercase font-semibold h-[60px] flex justify-evenly items-center">
+        <h3 className="text-xl">Total Items: {cart.length}</h3>
+        <h3 className="text-xl">Total Price: ${total}</h3>
+        <Link>
+          <button className="btn btn-warning btn-sm">PAY</button>
+        </Link>
       </div>
       <div>
         {cart && Array.isArray(cart) ? (
